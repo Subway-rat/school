6fe09c440a207e9395aa6adf9752365c6b220de1
Subway-rat <42968676+Subway-rat@users.noreply.github.com>
2018-09-07 12:13:27 +0200

    Update ball_table3.py

--- ball_table3.py
+++ ball_table3.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep  1 10:38:39 2018
-
 @author: matsh
 """
+
 g = 9.81; v0 = 50; n = 5 #masse variabler
 t0 = [x*(2*v0/(n*g)) for x in range(0,n+1)] #definerer t
 y0 = [round((v0*i)-(0.5*g*i**2),2) for i in t0] #definerer y
@@ -13,13 +13,13 @@ ty1 = [t0,y0] #koloner
 #print(ty1)
 
 for x in range(len(ty1[0])):
-    print("{:^10} --> {:^10}".format(round(ty1[0][x],2), ty1[1][x]))
+    print("{:^10} --> {:^10}".format(round(ty1[0][x],2), ty1[1][x])) #printer ut kolonner
 
 #print(ty2)
 print("")
 
 for x in ty2:
-    print("{:^10} --> {:^10}".format(x[0], x[1]))
+    print("{:^10} --> {:^10}".format(x[0], x[1])) #printer ut rader
 
 #for z in ty1: #henter lister
 #    for u in z:       
@@ -28,7 +28,7 @@ for x in ty2:
 #        print("{}: {}".format(v,u)) #printer lister med 2 desimaler
 
 """
-execute: ball_table3.py
+>>> ball_table3.py
    0.0     -->    0.0    
    2.04    -->   81.55   
    4.08    -->   122.32  
