18fb579c24103fff4c296fcf2d6e15e71ba6149d
Subway-rat <42968676+Subway-rat@users.noreply.github.com>
2018-09-07 12:14:25 +0200

    Update sum_while.py

--- sum_while.py
+++ sum_while.py
@@ -1,5 +1,17 @@
-s = 0;k = 1;M = int(input("upper limit "))+1
-while k<M:
-    s += float(1/k)
-    k += 1.0
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 11:49:01 2018
+
+@author: matsh
+"""
+
+s = 0;k = 1;M = 100 #definerer variabler, M er øvre grense
+while k<(M+1): 
+    s += float(1/k) # summerer
+    k += 1.0 # går viderer til neste tall
 print (round(s,9))
+
+"""
+>>> sum_while.py
+5.187377518
+"""
